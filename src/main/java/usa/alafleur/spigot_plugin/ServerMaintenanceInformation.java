103bc92d13d62f6750029312f3422e70638493a8
Andre LaFleur <cincospenguinos@gmail.com>
2022-10-16 12:23:18 -0600

    Add .gitignore and continue implementation

--- src/main/java/usa/alafleur/spigot_plugin/ServerMaintenanceInformation.java
+++ src/main/java/usa/alafleur/spigot_plugin/ServerMaintenanceInformation.java
@@ -1,14 +1,34 @@
 package usa.alafleur.spigot_plugin;
 
+import org.bukkit.Server;
+
+/**
+ * Interface between the Bukkit server and anything that needs to know anything about
+ * the server. Does not handle commands; only provides information.
+ */
 public class ServerMaintenanceInformation {
-    private final long startupTime;
+    private final long _startupTime;
+    private Server _server;
 
     public ServerMaintenanceInformation() {
-        startupTime = System.currentTimeMillis();
+        _startupTime = System.currentTimeMillis();
     }
 
-    public long getMinutesSinceStartup() {
+    /**
+     * Number of minutes since this object was instantiated.
+     *
+     * @return int - minutes
+     */
+    public int getMinutesSinceStartup() {
         long now = System.currentTimeMillis();
-        return (now - startupTime) / (60L * 1000L);
+        return Math.toIntExact((now - _startupTime) / (60L * 1000L));
+    }
+
+    public int getTotalPlayersOnline() {
+        return _server.getOnlinePlayers().size();
+    }
+
+    public void setServer(Server server) {
+        _server = server;
     }
 }
--- src/test/java/usa/alafleur/spigot_plugin/ServerMaintenanceInformationTest.java
+++ src/test/java/usa/alafleur/spigot_plugin/ServerMaintenanceInformationTest.java
@@ -16,4 +16,17 @@ class ServerMaintenanceInformationTest {
         ServerMaintenanceInformation source = new ServerMaintenanceInformation();
         assertEquals(0, source.getMinutesSinceStartup());
     }
+
+    @Test
+    public void Test_IndicatesNumberOfPlayersOnline() {
+        MockServer server = new MockServer();
+        server.TotalOnlinePlayers = 0;
+        ServerMaintenanceInformation source = new ServerMaintenanceInformation();
+        source.setServer(server);
+
+        assertEquals(0, source.getTotalPlayersOnline());
+
+        server.TotalOnlinePlayers = 10;
+        assertEquals(10, source.getTotalPlayersOnline());
+    }
 }
